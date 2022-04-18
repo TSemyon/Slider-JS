8f3f4e704b6068e5e2571d8186eec3d4f852f229
Semyon Taraev <103914684+TSemyon@users.noreply.github.com>
2022-04-18 08:48:37 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+const upBtn = document.querySelector('.up-button')
+const downBtn = document.querySelector('.down-button')
+const sidebar = document.querySelector('.sidebar')
+
+const mainSlide = document.querySelector('.main-slide')
+const slidesCount = mainSlide.querySelectorAll('div').length
+
+const container = document.querySelector('.container')
+
+sidebar.style.top = `-${(slidesCount - 1) * 100}vh`
+
+let activeSlideIndex = 0
+
+upBtn.addEventListener('click', () => {
+    changeSlide('up')
+    
+})
+
+downBtn.addEventListener('click', () => {
+    changeSlide('down')
+})
+
+function changeSlide(direction) {
+    if (direction === 'up') {
+        activeSlideIndex++
+        if (activeSlideIndex === slidesCount) {
+            activeSlideIndex = 0
+        }
+        else if (direction === 'down') {
+            activeSlideIndex--
+            if (activeSlideIndex < 0) {
+                activeSlideIndex = slidesCount - 1
+            }
+        }
+    }
+
+    const height = container.clientHeight
+
+    mainSlide.style.transform = `traslateY(-${activeSlideIndex * height}px)`
+
+    sidebar.style.transform = `traslateY(${activeSlideIndex * height}px)`
+}
